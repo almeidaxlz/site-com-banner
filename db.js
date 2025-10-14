930832e2c1d39ca23e7c486160d8c610c1dd6eed
almeidaxlz <162449595+almeidaxlz@users.noreply.github.com>
2025-10-14 11:04:56 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise")
 // criar a conexão
 const pool = mysql.createPool({
     host: "127.0.0.1",
-    port: "3000",
+    port: "3306",
     user: "root",
-    password: "", //senha "escola" no linux
+    password: "escola", //senha "escola" no linux
     database: "api_epa"
 })
 module.exports = Object.freeze({
